a7c8c63bcd1b5d82978fb6404fe44b9171dd1ed9
Paul Hottell <paul.hottell@gmail.com>
2017-01-05 13:47:08 -0800

    Initial setup

---
+++ Pods/Target Support Files/Pods-hscs-social/Pods-hscs-social-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_hscs_socialVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_hscs_socialVersionString[];
+
---
+++ hscs-social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  hscs-social
+//
+//  Created by Paul Hottell on 1/5/17.
+//  Copyright © 2017 HSCS. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
