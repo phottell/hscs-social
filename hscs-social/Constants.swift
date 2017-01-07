89b92fed36f5122727dffcc9e896de93d8ab5afd
Paul Hottell <paul.hottell@gmail.com>
2017-01-07 14:49:20 -0800

    Sign-in UI custom classes complete

---
+++ hscs-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  hscs-social
+//
+//  Created by Paul Hottell on 1/7/17.
+//  Copyright © 2017 HSCS. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ hscs-social/CoolField.swift
@@ -0,0 +1,28 @@
+//
+//  CoolField.swift
+//  hscs-social
+//
+//  Created by Paul Hottell on 1/7/17.
+//  Copyright © 2017 HSCS. All rights reserved.
+//
+
+import UIKit
+
+class CoolField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 2.0
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+}
