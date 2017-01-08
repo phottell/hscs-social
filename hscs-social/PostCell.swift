678e4ca6e5108987b890272440efe0bb1a106ab3
Paul Hottell <paul.hottell@gmail.com>
2017-01-08 15:20:10 -0800

    Feed TableView setup complete

---
+++ hscs-social/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  hscs-social
+//
+//  Created by Paul Hottell on 1/8/17.
+//  Copyright © 2017 HSCS. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
