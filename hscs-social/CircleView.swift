e187dbd5da2a3544c786ba6568a1b0c04829277b
Paul Hottell <paul.hottell@gmail.com>
2017-01-08 14:52:27 -0800

    Initial Feed UI complete

--- hscs-social/CircleView.swift
+++ hscs-social/CircleView.swift
@@ -1,23 +1,24 @@
 //
-//  CoolView.swift
+//  CircleView.swift
 //  hscs-social
 //
-//  Created by Paul Hottell on 1/7/17.
+//  Created by Paul Hottell on 1/8/17.
 //  Copyright © 2017 HSCS. All rights reserved.
 //
 
 import UIKit
 
-class CoolView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
     }
     
+    override func layoutSubviews() {
+         layer.cornerRadius = self.frame.width / 2
+    }
 }
