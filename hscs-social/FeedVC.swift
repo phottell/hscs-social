e8b46d20d30a0ce364d8fe056d5f3bbbe6ba7e43
Paul Hottell <paul.hottell@gmail.com>
2017-01-09 15:46:17 -0800

    Completed making a post to Firebase

--- hscs-social/FeedVC.swift
+++ hscs-social/FeedVC.swift
@@ -112,11 +112,29 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
                 } else {
                     print("HSCS: Successfully uploaded image to Firebase storage")
                     let downloadUrl = metaData?.downloadURL()?.absoluteString
-                    
+                    if let url = downloadUrl {
+                        self.postToFirebase(imgUrl: url)
+                    }
                 }
             }
         }
+    }
+    
+    func postToFirebase(imgUrl: String) {
+        let post: Dictionary<String, Any> = [
+            "caption": captionField.text!,
+            "imageUrl": imgUrl,
+            "likes": 0
+        ]
         
+        let firebasePost = DataService.ds.REF_POSTS.childByAutoId()
+        firebasePost.setValue(post)
+        
+        captionField.text = ""
+        imageSelected = false
+        imageAdd.image = UIImage(named: "add-image")
+        
+        tableView.reloadData()
     }
     
     @IBAction func signOutTapped(_ sender: Any) {
